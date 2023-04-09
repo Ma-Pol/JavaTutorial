851602e37916c81a710bf13c86cba3416f132d38
Ma-Pol <ysh.skyblue6485@gmail.com>
2023-04-09 16:01:45 +0900

    #1 자료형 - 기본 자료형

--- Sparta_Java/lib/src/main/java/Sparta_Java/Main.java
+++ Sparta_Java/lib/src/main/java/Sparta_Java/Main.java
@@ -2,6 +2,19 @@ package Sparta_Java;
 
 public class Main {
 	public static void main(String[] args) {
-		
+		int number = 5;
+		System.out.println(number + "\n");
+
+		String sparta = "Hello, Sparta\n";
+		System.out.println(sparta);
+
+		String project = "나의 최애를 알아줘!\n";
+		System.out.println(project);
+
+		final int finalNumber = 1;
+		System.out.println(finalNumber + "\n");
+
+		sparta = "Good Bye Sparta";
+		System.out.println(sparta);
 	}
 }

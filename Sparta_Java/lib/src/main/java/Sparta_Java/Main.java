a2c6ac26e095cd14d21549e50f59d075e2af28a3
Ma-Pol <ysh.skyblue6485@gmail.com>
2023-04-09 16:50:20 +0900

    #4 자료형 - 참조 자료형, 배열

--- Sparta_Java/lib/src/main/java/Sparta_Java/Main.java
+++ Sparta_Java/lib/src/main/java/Sparta_Java/Main.java
@@ -1,21 +1,34 @@
 package Sparta_Java;
 
+import java.util.Arrays;
+
 public class Main {
 	public static void main(String[] args) {
 
-		// char: 문자
-		char a = 'A';
-		System.out.println(a);
+		// 참조 자료형: 기본 자료형이 아닌 모든 것. 객체를 가리키는 자료형.
+		String sparta = "Hello Sparta!";
+		System.out.println(sparta);
 
-		// boolean: 논리
-		boolean isTrue = true;
-		boolean isFalse = false;
-		System.out.println(isTrue);
-		System.out.println(isFalse);
+		int[] intArray = new int[] { 1, 2, 3, 4, 5 };
+		System.out.println(Arrays.toString(intArray) + "\n");
 
-		// byte: ASCII code
-		byte data = '*';
-		System.out.println(data);
+		// 배열
+		int[] intEmptyArray = new int[5];
+		intEmptyArray[1] = 4;
+//		System.out.println(intEmptyArray);
+		System.out.println(Arrays.toString(intEmptyArray));
+
+		int[] intArray2 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		System.out.println(Arrays.toString(intArray2));
+
+		String[] stringEmptyArray = new String[5];
+		stringEmptyArray[3] = "값1";
+		System.out.println(Arrays.toString(stringEmptyArray));
+
+		String[] stringArray = { "봄", "여름", "가을", "겨울" };
+		System.out.println(Arrays.toString(stringArray));
+
+		System.out.println(stringArray[stringArray.length - 1]);
 
 	}
 }
